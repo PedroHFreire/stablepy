af3efc9687c14d30584ec3d1bc578a7fceaf5fbc
Pedro Freire <phfreire@id.uff.br>
2022-12-18 12:28:31 -0300

    Initial commit of setup.py for stable package
    This commit includes the initial version of setup.py for the stable package. The setup.py file includes information about the package such as its name, version, and metadata. It also specifies that the package includes a single package called 'stable', and that it has no dependencies.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='stable',
+    version='1.0.0',
+    author='Pedro Freire',
+    author_email='hfreire.p@gmail.com',
+    coauthor='Assistant',
+    coauthor_email='assistant@openai.com',
+    description='A package for investment analysis',
+    packages=['stable'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
